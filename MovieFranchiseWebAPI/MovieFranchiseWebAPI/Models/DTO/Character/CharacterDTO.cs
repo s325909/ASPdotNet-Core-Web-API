955a7dcf47bb4ce43dc0862efd1f99bbab5e12ff
s325909 <jaso_cyril@hotmail.com>
2022-01-31 20:43:17 +0100

    Feat: added List<int> Movies to CharacterReadDTO + made new CharacterDTO for Create & Edit

--- MovieFranchiseWebAPI/MovieFranchiseWebAPI/Models/DTO/Character/CharacterDTO.cs
+++ MovieFranchiseWebAPI/MovieFranchiseWebAPI/Models/DTO/Character/CharacterDTO.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 
 namespace MovieFranchiseWebAPI.Models.DTO.Character
 {
-    public class CharacterReadDTO
+    public class CharacterDTO
     {
-        public int Id { get; set; }
         public string FullName { get; set; }
         public string Alias { get; set; }
         public string Gender { get; set; }
         public string PictureURL { get; set; }
-        public List<int> Movies { get; set; } 
     }
 }
