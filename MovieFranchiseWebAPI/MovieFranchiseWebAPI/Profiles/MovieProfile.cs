86f86cf5bc2f43110af73a68ff4cc3478a517c5e
s325909 <jaso_cyril@hotmail.com>
2022-01-31 21:38:13 +0100

    Feat: MovieProfile with AutoMapper

---
+++ MovieFranchiseWebAPI/MovieFranchiseWebAPI/Profiles/MovieProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MovieFranchiseWebAPI.Models.Domain;
+using MovieFranchiseWebAPI.Models.DTO.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFranchiseWebAPI.Profiles
+{
+    public class MovieProfile : Profile 
+    {
+        public MovieProfile()
+        {
+            // Movie --> MovieReadDTO
+            CreateMap<Movie, MovieReadDTO>()
+                // turning related characters into int array
+                .ForMember(mdto => mdto.Characters, opt => opt
+                .MapFrom(m => m.Characters.Select(m => m.Id).ToArray()))
+                // (int) Franchise <--> FranchiseId
+                .ForMember(mdto => mdto.Franchise, opt => opt
+                .MapFrom(m => m.FranchiseId))
+                .ReverseMap();
+            // MovieDTO --> Movie
+            CreateMap<MovieDTO, Movie>(); 
+        }
+    }
+}
