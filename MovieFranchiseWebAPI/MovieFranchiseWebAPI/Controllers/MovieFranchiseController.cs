57a533b73a4787649b201456a81d9792ba479579
s325909 <jaso_cyril@hotmail.com>
2022-01-31 13:26:54 +0100

    Feat: VS project with EF Core + SqlServer Installed
    MovieFranchiseController, MovieFranchiseContext & IMovieFranchiseRepo made and added to services at startup with swagger for testing purposes, with a default ConnectionString also added to appsetings.json for connection to SSMS

---
+++ MovieFranchiseWebAPI/MovieFranchiseWebAPI/Controllers/MovieFranchiseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieFranchiseWebAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFranchiseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieFranchiseController : ControllerBase
+    {
+        private IMovieFranchiseRepo _movieFranchise;
+        public MovieFranchiseController(IMovieFranchiseRepo movieFranchise)
+        {
+            _movieFranchise = movieFranchise;
+        }
+
+        [HttpGet]
+        // [Route("api/[controller]")]
+        public IActionResult GetEmployees()
+        {
+            //return Ok(_movieFranchise.GetMovies());
+            return Ok();
+        }
+    }
+}
---
+++ MovieFranchiseWebAPI/MovieFranchiseWebAPI/Models/MovieFranchiseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFranchiseWebAPI.Models
+{
+    public class MovieFranchiseContext : DbContext 
+    {
+        public MovieFranchiseContext(DbContextOptions<MovieFranchiseContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
